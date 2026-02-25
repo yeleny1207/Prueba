038cb16cfc68955728e0e6fa5be7a95a7e097e2b
Evelyn <yely.eve1299@gmail.com>
2026-02-25 12:01:12 -0500

    Prueba

---
+++ backend/views/site/login.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap4\ActiveForm $form */
+/** @var \common\models\LoginForm $model */
+
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+
+$this->title = 'Autenticacion';
+?>
+<div class="site-login">
+    <div class="mt-5 offset-lg-3 col-lg-6">
+        <h1><?= Html::encode($this->title) ?></h1>
+
+        <p>Por favor ingrese sus datos para autenticarse</p>
+
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'password')->passwordInput() ?>
+             <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+            <div class="form-group">
+                <?= Html::submitButton('Ingresar', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+            </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
